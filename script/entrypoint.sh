dbb13b843ce0dcc20a828baf79e4077a623dc886
Md Nadeem Ashraf Ansari <nansari1902@gmail.com>
2025-09-17 14:35:33 +0530

    add files via git-commit to main-branch

---
+++ script/entrypoint.sh
@@ -0,0 +1,24 @@
+# Author: Md Nadeem Ansari
+# Date: 15-09-2025
+
+set -e
+
+if [ -e "/opt/airflow/requirements.txt" ]; then
+  $(command python) pip install --upgrade pip
+  $(command -v pip) install --user -r requirements.txt
+fi
+
+if [ ! -f "/opt/airflow/airflow.db" ]; then
+  airflow db init && \
+  airflow users create \
+    --username admin \
+    --firstname admin \
+    --lastname admin \
+    --role Admin \
+    --email admin@example.com \
+    --password admin
+fi
+
+$(command -v airflow) db upgrade
+
+exec airflow webserver
